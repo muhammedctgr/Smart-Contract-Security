792dc0b437581eee1c4d9f187b16bb5265c44389
unknown <muhammedko420@gmail.com>
2025-12-26 13:33:50 +0100

    Test

--- src/CaughtWithTest.sol
+++ src/CaughtWithTest.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.13;
 contract CaughtWithTest {
     uint256 public number;
 
- 
+    function setNumber(uint256 newNumber) public {
+        // Whoops, this isn't right!
+        number = newNumber + 1;
+    }
 }
 
